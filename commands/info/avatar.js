1caa8f871f52e607bfa7d86d9c25b60a17fae356
Glitch (curvy-verbena-whippet) <none>
2020-07-29 12:07:04 +0000

    🎍💪 Checkpoint ./commands/fun/meme.js:16543689/305 ./commands/fun/neko.js:16543689/307 ./commands/fun/pat.js:16543689/594 ./commands/fun/punch.js:16543689/604 ./commands/fun/slap.js:16543689/598 ./CODE_OF_CONDUCT.md:16543689/6761 ./LICENSE.md:16543689/1075 ./README.md:16543689/1040 ./.glitch-assets:16543689/526 ./commands/info/avatar.js:16543689/888

---
+++ commands/info/avatar.js
@@ -0,0 +1,29 @@
+const discord = require("discord.js")
+
+module.exports = {
+  name: "avatar",
+  aliases: ["av"],
+  category: "info",
+  description: "Get dp of any user",
+  run: async (client, message, args) => {
+    
+    let embed = new discord.MessageEmbed()
+    
+    if(!message.mentions.users.first()){
+      embed.setDescription(`[Download](${message.author.displayAvatarURL({ dynamic: true, size: 1024 })})`)
+      embed.setImage(message.author.displayAvatarURL({ dynamic: true, size: 1024 }))
+      embed.setColor("RANDOM")
+      message.channel.send(embed)
+      
+    } else {
+      
+      let target = message.mentions.users.first()
+      
+      embed.setDescription(`[Download](${target.displayAvatarURL({ dynamic: true, size: 1024 })})`)
+      embed.setImage(target.displayAvatarURL({ dynamic: true, size: 1024 }))
+      embed.setColor("RANDOM")
+      message.channel.send(embed)
+    
+    }
+  }
+}
