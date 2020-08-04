076e549584b1ec66241c2b774cfc472422a3eee9
Glitch (indian-v1-0-2) <none>
2020-08-04 13:48:07 +0000

    🚏🎱 Checkpoint ./commands/info/avatar.js:16543689/499

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -9,21 +9,12 @@ module.exports = {
     
     let embed = new discord.MessageEmbed()
     
-    if(!message.mentions.users.first()){
-      embed.setDescription(`[Download](${message.author.displayAvatarURL({ dynamic: true, size: 1024 })})`)
-      embed.setImage(message.author.displayAvatarURL({ dynamic: true, size: 1024 }))
-      embed.setColor("RANDOM")
-      message.channel.send(embed)
-      
-    } else {
-      
-      let target = message.mentions.users.first()
+      let target = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author.id
       
       embed.setDescription(`[Download](${target.displayAvatarURL({ dynamic: true, size: 1024 })})`)
       embed.setImage(target.displayAvatarURL({ dynamic: true, size: 1024 }))
       embed.setColor("RANDOM")
       message.channel.send(embed)
-    
-    }
+      
   }
 }
