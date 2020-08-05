9cd09b5dd5c8d7fd1fdbd7c8d4bdc7ae7da3d566
Glitch (lackadaisical-smiling-stone) <none>
2020-08-05 04:12:10 +0000

    🌠🏅 Checkpoint ./commands/info/avatar.js:16543689/1155

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -8,13 +8,21 @@ module.exports = {
   run: async (client, message, args) => {
     
       let embed = new discord.MessageEmbed()
-    
-      let target = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author.id
       
-      embed.setDescription(`[Download](${target.user.displayAvatarURL({ dynamic: true, size: 1024 })})`)
-      embed.setImage(target.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+      if(!message.mentions.users.first()) {
+        embed.setDescription(`[Download](${message.author.displayAvatarURL({ dynamic: true, size: 1024 })})`)
+        embed.setImage(message.author.displayAvatarURL({ dynamic: true, size: 1024 }))
+        embed.setColor("RANDOM")
+        message.channel.send(embed)
+        
+      } else {
+        
+     let target = message.mentions.users.first()
+      embed.setDescription(`[Download](${target.displayAvatarURL({ dynamic: true, size: 1024 })})`)
+      embed.setImage(target.displayAvatarURL({ dynamic: true, size: 1024 }))
       embed.setColor("RANDOM")
       message.channel.send(embed)
-      
+        
+      } 
   }
 }
