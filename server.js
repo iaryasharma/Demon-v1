45ebc9aff01248d108d257ea240434a1f3180a5c
Glitch (petalite-florentine-dogsled) <none>
2020-08-15 15:28:24 +0000

    👔🙉 Checkpoint ./covid.js:20997192/1460 ./package.json:20997192/2388 ./server.js:20997192/25

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const client = new discord.Client({
 });
 require("./music.js");
 require("./uptime.js");
+require("./covid.js");
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
 
