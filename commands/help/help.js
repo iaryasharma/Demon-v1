f4eaea3c56a5eb097fbb1edb930925052c9941cc
Glitch (petalite-florentine-dogsled) <none>
2020-08-16 14:03:52 +0000

    🎥🍽 Checkpoint ./commands/moderation/mute.js:20997192/1435 ./commands/help/help.js:20997192/826

--- commands/help/help.js
+++ commands/help/help.js
@@ -9,31 +9,10 @@ module.exports = {
       .setTitle(
         `<a:cs:731484346124468236>__**PARAS BOT'S DASHBOARD**__<a:cs:731484346124468236>`
       )
-      .setDescription(
-        `
-**MODERATION FEATURES**
-\`warn,mute,softban,ban,kick,clear,lockdown,addrole,removerole\`
-
-<a:cc_flame:731435785617670208>**UTILITY FEATURES**<a:cc_flame:731435785617670208>
-\`avatar,serverinfo,botinfo,ping,stats,uptime,wiki,weather,date\`
-
-<a:cc_flame:731435785617670208>**FUN FEATURES**<a:cc_flame:731435785617670208>
-\`fact,meme,cat,dog,seal,pepe,alpaca,hug,highfive,holdhand,kill\`
-
-<a:cc_flame:731435785617670208>**GAMING FEATURES **<a:cc_flame:731435785617670208>
-\`8ball,complication,love,reverse,roast,rps,joke,cnjoke\`
-
-<a:cc_flame:731435785617670208>**MUSIC FEATURES **<a:cc_flame:731435785617670208>
-\`play,search,skip,stop,pause,resume,nowplaying,queue,volume\`
-
-**SPECIAL FEATURES**
-\`GIVEAWAY \`
-
-
-`
-      )
+      .setDescription(`\```HI\```
+`)
       .setColor("RANDOM")
-      .setFooter(`DASHBOARD OF PARAS BOT`)
+      .setFooter(`PARAS BOT`)
       .setTimestamp((message.timestamp = Date.now()));
 
     message.channel.send(embed);
