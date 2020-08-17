73f3dedcba17c13e3ba78a07d4204c2ce87349c5
Glitch (petalite-florentine-dogsled) <none>
2020-08-17 17:39:40 +0000

    💫🎮 Checkpoint ./commands/help/announce.js:20997192/29

--- commands/help/announce.js
+++ commands/help/announce.js
@@ -8,7 +8,7 @@ category: "Utility",
 usage: "embed <text to say>",
 description: "Returns provided text in Embed form.",
 run: async(client, message, args) => {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`YOU DO NOT HAVE PERMISSION `)
+  if(!message.member.hasPermission("ADMINISTRATION")) return message.channel.send(`YOU DO NOT HAVE PERMISSION `)
  await message.delete()
   let say = message.content.split(" ").slice(1).join(" ")
   if(!say) return message.channel.send(`❌ | `+"I Cannot Repeat Blank Message")
