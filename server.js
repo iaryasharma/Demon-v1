058ef0d4710fb8d4f9a823219687a55dde4e0c88
Glitch (petalite-florentine-dogsled) <none>
2020-08-18 17:07:17 +0000

    🕌🏛 Checkpoint ./server.js:20997192/1425

--- server.js
+++ server.js
@@ -47,6 +47,55 @@ client.on("message", async message => {
   if (!command) command = client.commands.get(client.aliases.get(cmd));
 
   if (command) command.run(client, message, args);
+});const { prefix } = require("./config.json");
+const { config } = require("dotenv");
+const db =require("quick.db");
+const { CanvasSenpai } = require("canvas-senpai")
+const canva = new CanvasSenpai();
+const discord = require("discord.js");
+const client = new discord.Client({
+  disableEveryone: false
+});
+require("./music.js");
+require("./uptime.js");
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+
+["command"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+});
+
+client.on("ready", () => {
+  console.log(` ${client.user.username} is turned on`);
+  client.user.setActivity(`P!help | P!invite`,{type:"WATCHING"});
+});
+
+client.on("message", async message => {
+  const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
+  if (message.content.match(prefixMention)) {
+    return message.reply(`My prefix is \`${prefix}\``);
+  }
+
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd.length === 0) return;
+
+  let command = client.commands.get(cmd);
+
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  if (command) command.run(client, message, args);
 });
 
 
