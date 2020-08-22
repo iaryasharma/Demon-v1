46706777e67b6becfb7b6b92d35e4b8688aade98
Glitch (petalite-florentine-dogsled) <none>
2020-08-22 08:00:30 +0000

    🚪😚 Checkpoint ./commands/help/help.js:18736994/162 ./package.json:18736994/2769

--- commands/help/help.js
+++ commands/help/help.js
@@ -10,16 +10,19 @@ module.exports = {
       .setDescription(`
 <a:drl:730449009902223402> __**MODERATION COMMANDS**__<a:drl:730449009902223402>
 \`addrole,removerole,cleaer[purge],ban,kick,voicekick,mute,unmute,slowmode,warn,warnings,resetwarns\`
+
                                                     <a:drl:730449009902223402> __**FUN COMMANDS**__<a:drl:730449009902223402>
                                                                       \`advice,suggest,ascii,cat,cry,dog,fact,hug,joke,kiss,kpop,math,meme,mw,neko,pat,punch,slap\`
+
 <a:drl:730449009902223402> __**INFORMATION COMMANDS**__<a:drl:730449009902223402>
 \`avatar,botinfo,serverinfo,hastebin,imdb,membercount,ping,pokemon,servericon,,whois,userinfo\`
-> __**MUSIC COMMANDS**__
+
+<a:drl:730449009902223402> __**MUSIC COMMANDS**__<a:drl:730449009902223402>
 \`play,search,skip,stop,pause,resume,nowplaying,queue,volume\`
 
-> __**WELCOME COMMANDS**__
+<a:drl:730449009902223402> __**WELCOME COMMANDS**__<a:drl:730449009902223402>
 \`setwelcome,setmessage\`
-> __**SPECIAL COMMANDS**__
+<a:drl:730449009902223402> __**SPECIAL COMMANDS**__<a:drl:730449009902223402>
 \`giveaway,math,announce,emoji,\`
  
 NEW COMMAND SOON LIKE RANK SYSTEM,RADIO,GLOBAL CHAT
