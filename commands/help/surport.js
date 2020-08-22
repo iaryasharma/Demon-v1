269c557352257d0929d0f9e6ca40e99920be7c25
Glitch (petalite-florentine-dogsled) <none>
2020-08-22 06:50:31 +0000

    🚃🖖 Checkpoint ./commands/info/corona.js:18736994/990 ./commands/help/surport.js:18736994/735

---
+++ commands/help/surport.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "invite",
+  category: "help",
+  description: "INVITE SENPAI BOT",
+  run: async (client, message, args) => {
+    
+    let embed = new discord.MessageEmbed()
+    .setTitle(`SURPORT SERVER FOR SENPAI OP`)
+    .setDescription(`[CLICK HERE](https://discord.com/api/oauth2/authorize?client_id=746361678840725644&permissions=8&scope=bot)`)
+    .setColor("RANDOM")
+    .setFooter(`BOT MADE BY KANEKI`)
+    .setTimestamp(message.timestamp = Date.now())
+    
+    message.channel .send(embed)
+     
+  
+  }
+}     
