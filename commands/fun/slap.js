78ecbd34278f149916b6cbb4d87979d1469fdf5f
Glitch (petalite-florentine-dogsled) <none>
2020-08-22 04:13:50 +0000

    🐕💝 Checkpoint ./commands/fun/slap.js:18736994/20287 ./server.js:18736994/20

--- commands/fun/slap.js
+++ commands/fun/slap.js
@@ -8,14 +8,16 @@ module.exports = {
   description: "Slap someone",
   run: async (client, message, args) => {
     
-   
+    let target = message.mentions.members.first()
+    
+    let data = await random.getAnimeImgURL("slap");
     
     let embed = new discord.MessageEmbed()
-    .setImage()
+    .setImage(data)
     .setColor("RANDOM")
     .setFooter(`${message.author.username} slaps ${target.user.username}`)
     .setTimestamp()
     
     message.channel.send(embed);
-  } 
-};
+  }
+}; 
