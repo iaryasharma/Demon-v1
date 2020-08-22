7f95735ac107e65691763194f37e5df3e239d541
Glitch (petalite-florentine-dogsled) <none>
2020-08-22 11:42:58 +0000

    🗼👞 Checkpoint ./commands/help/help.js:18736994/351

--- commands/help/help.js
+++ commands/help/help.js
@@ -8,25 +8,25 @@ module.exports = {
     let embed = new discord.MessageEmbed()
       .setTitle(`<a:mb:731368836846321694> SENPAI BOT  HELP MENU `)
       .setDescription(`
-<a:arrowright:746687100178661396> __**MODERATION COMMANDS**__ 
-\`addrole,removerole,cleaer[purge],ban,kick,voicekick,mute,unmute,slowmode,warn,warnings,resetwarns\`
+<a:arrowright:746687100178661396> __**MODERATION COMMANDS**__ <a:larrow:746692948057522217>
+\`addrole,removerole,cleaer[purge],ban,kick,voicekick,mute,unmute,slowmode,warn,warnings,resetwarns\`<a:verified:746693603710992404>
 
-<a:arrowright:746687100178661396> __**FUN COMMANDS**__ 
-\`advice,suggest,ascii,cat,cry,dog,fact,hug,joke,kiss,kpop,math,meme,mw,neko,pat,punch,slap\`
+<a:arrowright:746687100178661396> __**FUN COMMANDS**__ <a:larrow:746692948057522217>
+\`advice,suggest,ascii,cat,cry,dog,fact,hug,joke,kiss,kpop,math,meme,mw,neko,pat,punch,slap\`<a:fun:746693872893165569>
 
-<a:arrowright:746687100178661396> __**INFORMATION COMMANDS**__ 
-\`avatar,botinfo,serverinfo,hastebin,imdb,membercount,ping,pokemon,servericon,,whois,userinfo\`
+<a:arrowright:746687100178661396> __**INFORMATION COMMANDS**__ <a:larrow:746692948057522217>
+\`avatar,botinfo,serverinfo,hastebin,imdb,membercount,ping,pokemon,servericon,,whois,userinfo\`<a:info:746693700939153488>
 
-<a:arrowright:746687100178661396> __**MUSIC COMMANDS**__ 
+<a:arrowright:746687100178661396> __**MUSIC COMMANDS**__ <a:larrow:746692948057522217>
 \`play,search,skip,stop,pause,resume,nowplaying,queue,volume\`
 
-<a:arrowright:746687100178661396> __**NSFW COMMANDS**__ 
+<a:arrowright:746687100178661396> __**NSFW COMMANDS**__ <a:larrow:746692948057522217>
 \`nahi hai\`
 
-<a:arrowright:746687100178661396> __**WELCOME COMMANDS**__ 
+<a:arrowright:746687100178661396> __**WELCOME COMMANDS**__ <a:larrow:746692948057522217>
 \`setwelcome,setmessage\`
 
-<a:arrowright:746687100178661396> __**SPECIAL COMMANDS**__ 
+<a:arrowright:746687100178661396> __**SPECIAL COMMANDS**__ <a:larrow:746692948057522217>
 \`giveaway,math,announce,emoji,\`
  
 NEW COMMAND SOON LIKE RANK SYSTEM,RADIO,GLOBAL CHAT,SOME COMMANDS ARE NOT WORKING 
