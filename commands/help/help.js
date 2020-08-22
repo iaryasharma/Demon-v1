0746631346c23d471602f55b19b3d9dcc93c3335
Glitch (petalite-florentine-dogsled) <none>
2020-08-22 07:49:26 +0000

    🏊🏼🌨 Checkpoint ./commands/info/rank.js:18736994/2138 ./commands/help/help.js:18736994/2866

--- commands/help/help.js
+++ commands/help/help.js
@@ -6,18 +6,17 @@ module.exports = {
   description: "BOT GET SOON UPDATES ",
   run: async (client, message, args) => {
     let embed = new discord.MessageEmbed()
-      .setTitle(`SENPAI HELP MENU`)
+      .setTitle(`PARAS BOT  HELP MENU`)
       .setDescription(`
-> __**MODERATION COMMANDS**__
-\`addrole,removerole,clear[purge],ban,kick,voicekick,mute,unmute,slowmode,warn,warnings,resetwarns\`
-> __**FUN COMMANDS**__
-\`advice,suggest,ascii,cat,cry,dog,fact,hug,joke,kiss,kpop,math,meme,mw,neko,pat,punch,slap\`
-> __**INFORMATION COMMANDS**__
-\`avatar,botinfo,serverinfo,hastebin,imdb,author,membercount,ping,pokemon,servericon,,whois,userinfo\`
+<a:drl:730449009902223402> __**MODERATION COMMANDS**__<a:drl:730449009902223402>
+\`addrole,removerole,cleaer[purge],ban,kick,voicekick,mute,unmute,slowmode,warn,warnings,resetwarns\`
+                                                    <a:drl:730449009902223402> __**FUN COMMANDS**__<a:drl:730449009902223402>
+                                                                      \`advice,suggest,ascii,cat,cry,dog,fact,hug,joke,kiss,kpop,math,meme,mw,neko,pat,punch,slap\`
+<a:drl:730449009902223402> __**INFORMATION COMMANDS**__<a:drl:730449009902223402>
+\`avatar,botinfo,serverinfo,hastebin,imdb,membercount,ping,pokemon,servericon,,whois,userinfo\`
 > __**MUSIC COMMANDS**__
 \`play,search,skip,stop,pause,resume,nowplaying,queue,volume\`
-> __**NSFW COMMANDS**__
-\`JYADA CHARVI CHADI HAI\`
+
 > __**WELCOME COMMANDS**__
 \`setwelcome,setmessage\`
 > __**SPECIAL COMMANDS**__
@@ -28,7 +27,7 @@ NEW COMMAND SOON LIKE RANK SYSTEM,RADIO,GLOBAL CHAT
 
       .setThumbnail(client.user.displayAvatarURL())
       .setColor("#35CB31")
-      .setFooter(`SENPAI BOT `)
+      .setFooter(`PARAS BOT `)
       .setTimestamp((message.timestamp = Date.now()));
 
     await message.channel.send(embed);
