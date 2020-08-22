22594c9b53c711285756ad4f3b7b6ea1a4e359ca
Glitch (petalite-florentine-dogsled) <none>
2020-08-22 03:03:43 +0000

    🌸🍢 Checkpoint ./commands/help/author.js:18736994/1008

---
+++ commands/help/author.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "author",
+  category: "help",
+  description: "KNOW ABOUT THE MAKER OF BOT",
+  run: async (client, message, args) => {
+    
+    let embed = new discord.MessageEmbed()
+    .setTitle(`ABOUT KANEKI`)
+    .setDescription(`HI ＩＣＸ ॥乛KΛПΣKIᴰᴱᶻ ⏦#6063 IS THE CREATOR OF THE BOT 
+HE IS A PROFSSIONAL SERVER DESIGNER, MANAGER, BOT DEVELOPER. MAINLY HE USES JS TO CREATE BIT IF YOU WANT TO CONTACT HIM SEND A FRIEND REQUEST ON ＩＣＸ ॥乛KΛПΣKIᴰᴱᶻ ⏦#6063`)
+    .setColor("RANDOM")
+    .setFooter(`BOT MADE BY KANEKI`)
+    .setTimestamp(message.timestamp = Date.now())
+    
+    message.channel .send(embed)
+     
+  
+  }
+}    
