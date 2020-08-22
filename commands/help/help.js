64ffebd76639f42037aa04b210e8b972e2cf0ad4
Glitch (petalite-florentine-dogsled) <none>
2020-08-22 10:09:46 +0000

    ⏱👷 Checkpoint ./commands/help/idp.js:18736994/64 ./commands/help/help.js:18736994/234 ./commands/info/botinfo.js:18736994/100

--- commands/help/help.js
+++ commands/help/help.js
@@ -6,29 +6,35 @@ module.exports = {
   description: "BOT GET SOON UPDATES ",
   run: async (client, message, args) => {
     let embed = new discord.MessageEmbed()
-      .setTitle(`PARAS BOT  HELP MENU`)
+      .setTitle(`<a:mb:731368836846321694> SENPAI BOT  HELP MENU <a:mb:731368836846321694>`)
       .setDescription(`
 > __**MODERATION COMMANDS**__
 \`addrole,removerole,cleaer[purge],ban,kick,voicekick,mute,unmute,slowmode,warn,warnings,resetwarns\`
+
 > __**FUN COMMANDS**__
 \`advice,suggest,ascii,cat,cry,dog,fact,hug,joke,kiss,kpop,math,meme,mw,neko,pat,punch,slap\`
+
 > __**INFORMATION COMMANDS**__
 \`avatar,botinfo,serverinfo,hastebin,imdb,membercount,ping,pokemon,servericon,,whois,userinfo\`
-> __**MUSIC COMMANDS**__<a:cc_heart:742232464109338638>
+
+> __**MUSIC COMMANDS**__
 \`play,search,skip,stop,pause,resume,nowplaying,queue,volume\`
+
 > __**NSFW COMMANDS**__
 \`nahi hai\`
+
 > __**WELCOME COMMANDS**__
 \`setwelcome,setmessage\`
+
 > __**SPECIAL COMMANDS**__
 \`giveaway,math,announce,emoji,\`
  
-NEW COMMAND SOON LIKE RANK SYSTEM,RADIO,GLOBAL CHAT
+NEW COMMAND SOON LIKE RANK SYSTEM,RADIO,GLOBAL CHAT,SOME COMMANDS ARE NOT WORKING 
 `)
 
       .setThumbnail(client.user.displayAvatarURL())
       .setColor("#35CB31")
-      .setFooter(`PARAS BOT `)
+      .setFooter(`SENPAI BOT `)
       .setTimestamp((message.timestamp = Date.now()));
 
     await message.channel.send(embed);
