a12b7d4185245f884872d5d70ca21fa66bb70927
Glitch (petalite-florentine-dogsled) <none>
2020-08-24 05:39:23 +0000

    🚝🎣 Checkpoint ./commands/fun/cry.js:18736994/875

--- commands/fun/cry.js
+++ commands/fun/cry.js
@@ -1,16 +1,22 @@
-const { Random } = require("something-random-on-discord")
-const random = new Random();
+  
+const discord = require("discord.js");
+const { Random } = require("something-random-on-discord");
+const random = new Random();
 
 module.exports = {
-name: "nekocry",
-category: "fun",
-usage: "nekocry", 
-description: "Get fresh Neko cry image :D",
-run: async (client, message, args) => {
-
-let data = await random.getAnimeImgURL("cry")
-    message.channel.send(data)
-
-
-}
-}
+  name: "cry",
+  category: "fun",
+  description: "Cry with gif",
+  run: async (client, message, args) => {
+    
+    let data = await random.getAnimeImgURL("cry");
+    
+    let embed = new discord.MessageEmbed()
+    .setImage(data)
+    .setColor("RANDOM")
+    .setFooter(`Please talk with ${message.author.username} they are crying`)
+    .setTimestamp()
+    
+    message.channel.send(embed);
+  }
+};
