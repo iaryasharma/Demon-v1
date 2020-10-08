20024d61b7f7b31a4492d90b7ff10b95078c7aa3
Glitch (pine-mica-spy) <none>
2020-10-08 07:24:26 +0000

    🛢👨‍❤️‍👨 Checkpoint ./commands/help/author.js:23410437/16

--- commands/help/author.js
+++ commands/help/author.js
@@ -3,13 +3,13 @@ const discord = require("discord.js");
 module.exports = {
   name: "author",
   category: "help",
-  description: "KNOW ABOUT THE MAKER OF BOT",
+  description: "KNOW ABOUT THE CREATOR OF BOT",
   run: async (client, message, args) => {
     
     let embed = new discord.MessageEmbed()
     .setTitle(`ABOUT SHADOW`)
-    .setDescription(``THE CREATOR OF THE BOT Is ShaDoW
-HE IS A PROFSSIONAL SERVER DESIGNER, MANAGER, BOT DEVELOPER. MAINLY HE USES JS TO CREATE BOT``)
+    .setDescription(`_THE CREATOR OF THE BOT Is ShaDoW
+HE IS A PROFSSIONAL SERVER DESIGNER, MANAGER, BOT DEVELOPER. MAINLY HE USES JS TO CREATE BOT_`)
     .setColor("RANDOM")
     .setFooter(`BOT MADE BY ShaDoW & Venom`)
     .setTimestamp(message.timestamp = Date.now())
