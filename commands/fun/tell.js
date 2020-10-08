550681b113e8c6f45fb0f6693e01b44c58a60b66
Glitch (pine-mica-spy) <none>
2020-10-08 05:37:24 +0000

    💺👢 Checkpoint ./commands/fun/tell.js:23410437/3679

--- commands/fun/tell.js
+++ commands/fun/tell.js
@@ -1,31 +1,19 @@
 const Discord = require("discord.js");
-
 module.exports = {
   name: "tell",
-
   desciption: "say command",
-
   category: "fun",
-
-  usage: "tell #channel <message>",
-
+  usage: "tell <message>",
   async run(client, message, args) {
     const sayMessage = args.join(" ");
-
-    const textChannel = message.mentions.channels.first();
-
     message.delete().catch(err => console.log(err));
 
     const embed = new Discord.MessageEmbed()
-
       .setColor("RANDOM")
-
       .setDescription(sayMessage)
-
       .setTimestamp();
-
     try {
-      message.textMessage.channel.send(embed);
+      message.channel.send(embed);
     } catch {
       message.reply("Something Went Wrong");
     }
