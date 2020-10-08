27b1166044010c8ebcda7d4a6974a1f156af585b
Glitch (pine-mica-spy) <none>
2020-10-08 11:55:52 +0000

    💝📮 Checkpoint ./commands/fun/dm.js:23410437/2087

--- commands/fun/dm.js
+++ commands/fun/dm.js
@@ -2,15 +2,22 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "dm",
+
   description: "DM Mentioned User",
-  execute(message, args) {
+
+  run(message, args) {
     const dmMsg = args.join(" ");
+
     message.delete().catch(err => console.log(err));
 
     const dm = new Discord.MessageEmbed()
+
       .setColor("RANDOM")
+
       .setDescription(dmMsg)
+
       .setTimestamp();
+
     try {
       message.mentions.users.first().send(dm);
     } catch {
