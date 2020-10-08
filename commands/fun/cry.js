cd018b9d7035ebcd9ce787cce852c72719bd0eb4
Glitch (pine-mica-spy) <none>
2020-10-08 09:35:28 +0000

    ⛷👚 Checkpoint ./commands/fun/cry.js:23410437/1089

--- commands/fun/cry.js
+++ commands/fun/cry.js
@@ -1,4 +1,3 @@
-  
 const discord = require("discord.js");
 const { Random } = require("something-random-on-discord");
 const random = new Random();
@@ -8,15 +7,14 @@ module.exports = {
   category: "fun",
   description: "Cry with gif",
   run: async (client, message, args) => {
-    
     let data = await random.getAnimeImgURL("cry");
-    
+
     let embed = new discord.MessageEmbed()
-    .setImage(data)
-    .setColor("RANDOM")
-    .setFooter(`Please talk with ${message.author.username} they are crying`)
-    .setTimestamp()
-    
+      .setImage(data)
+      .setColor("RANDOM")
+      .setFooter(`Please talk with ${message.author.username} they are crying`)
+      .setTimestamp();
+
     message.channel.send(embed);
   }
 };
