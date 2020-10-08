22064860e5eadf579fb03e0fa31d6921b17c7461
Glitch (pine-mica-spy) <none>
2020-10-08 13:05:56 +0000

    🍗👩‍❤️‍💋‍👩 Checkpoint ./commands/info/ping.js:23410437/1 ./commands/info/avatar.js:23410437/4821

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 
 module.exports = {
-  name: "ping",
+  name: ".ping",
   category: "info",
   description: "Returns latency and API ping",
   run: async (client, message, args) => {
