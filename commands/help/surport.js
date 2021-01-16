43b2144bf5a8262d64d5c151985c71d5ab5c38ce
Glitch (hospitable-polite-cheetah) <none>
2021-01-16 11:53:12 +0000

    🐓🏙 Checkpoint ./commands/help/surport.js:26915577/116

--- commands/help/surport.js
+++ commands/help/surport.js
@@ -7,7 +7,7 @@ module.exports = {
     
     let embed = new discord.MessageEmbed()
     .setTitle(`SUPPORT SERVER`)
-    .setDescription(`[CLICK HERE](https://discord.gg/cfFyeMNKwx)`)
+    .setDescription(`[CLICK HERE](https://discord.gg/3UBbAsyJ2W)`)
     .setColor("RANDOM")
     .setFooter(`Mr. Wow`)
     .setTimestamp(message.timestamp = Date.now())
