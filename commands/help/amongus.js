f14f9b0d3074f4bec4edf7abc146af192f3d2867
Glitch (hospitable-polite-cheetah) <none>
2021-01-28 04:38:45 +0000

    🐫🚗 Checkpoint ./commands/help/idp.js copy:26900934/568 ./commands/help/amongus.js:26900934/703

---
+++ commands/help/amongus.js
@@ -0,0 +1,26 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "amongus",
+  run: async (client, message, args) => {
+    
+    if(!args[0]) {
+      return message.channel.send("🌏 Please Provide Server")
+    }
+    
+    let pass = args.slice(1).join(" ")
+    
+    if(!pass) {
+      return message.channel.send("🌌 Please Provide Code");
+    }
+
+  let embed = new discord.MessageEmbed()
+  .addField("🌏 Server", "`" + args[0] + "`")
+  .addField("🔑 Password", "`" + pass + "`")
+  .setColor("RANDOM")
+  message.channel.send(embed)
+
+  message.delete()
+    
+  }
+}
