cd1f8279f7fe0cda3311aa5d2d647cf26db518c8
Glitch (frag-nite-demon) <none>
2021-02-15 07:32:27 +0000

    Rewound to commit 7156c8a2af82175d70c049e92a3310722954f232 + 0 edits

--- commands/info/whois.js
+++ commands/info/whois.js
@@ -25,6 +25,9 @@ module.exports = {
       target.presence.status = "Online <a:GC_online1:810010059446812683>";
     if (target.presence.status === "offline")
       target.presence.status = "Offline <a:GC_offline1:810010019084107787>";
+    if (target.flags.toArray === "HOUSE_BRILLIANCE")
+      target.flags.toArray = "Brilliance <:brilliance:810581344077348915>";
+
     function game() {
       let game;
       if (target.presence.activities.length >= 1)
@@ -48,12 +51,9 @@ module.exports = {
     let aicon = message.author.avatarURL({ dynamic: true, size: 2048 });
     let createdate = moment.utc(target.createdAt).format("ddd, Do MMMM YYYY");
     let joindate = moment.utc(target.joinedAt).format("ddd, Do MMMM YYYY");
-    let flags = (await target.user.fetchFlags())
-        .toArray()
-        .map((flag) => badges[flag])
-        .join(" ");
+    let flags = target.flags.toArray();
+    if (target.flags.toArray() < 1) flags = "None";
 
-Badges: ${ ${flags}.length > 0 ? ${flags}: "None"});
     const embed = new MessageEmbed()
       .setAuthor(target.tag, avatar)
       .setThumbnail(avatar)
