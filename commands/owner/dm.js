089a018548d30df92d80ee218d535712aa60ce4c
Glitch (frag-nite-demon) <none>
2021-03-08 13:56:52 +0000

    🐂🐜 Checkpoint ./commands/owner/dm.js:26900934/742

--- commands/owner/dm.js
+++ commands/owner/dm.js
@@ -1,26 +1,25 @@
 const Discord = require("discord.js");
-
+const { prefix, owner } = require("../../config.json");
 module.exports = {
   name: "dm",
   description: "DM Mentioned User",
-
   async run(client, message, args) {
+    if (message.author.id !== owner) return;
     const dmMsg = args.join(" ");
-
-    message.delete().catch(err => console.log(err));
-
     const dm = new Discord.MessageEmbed()
-
+      //   const dm = new
+      //    const per =
+      //    message.mentions.users.first() || client.users.cache.get(args[0]);
+      //  if (per.id == "756052319417925633") return;
       .setColor("RANDOM")
-
       .setDescription(dmMsg)
-
       .setTimestamp();
+    message.delete();
 
     try {
       message.mentions.users.first().send(dm);
     } catch {
-      message.reply("Couldn'DM that person they have closed their dms of either have changed privacy settings.");
+      message.channel.send("Error Sending DM");
     }
   }
 };
