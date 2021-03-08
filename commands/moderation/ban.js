eabd1562b2e6d7a142412b788abcc2603a3e8c81
Glitch (frag-nite-demon) <none>
2021-03-08 14:17:08 +0000

    👡🌷 Checkpoint ./commands/owner/dm.js:28544438/306 ./commands/moderation/ban.js:28544438/79

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -46,7 +46,7 @@ module.exports = {
       let embed = new discord.MessageEmbed()
         .setColor("#ff0000")
         .setTitle(`Member Banned!`)
-        .addField(`Moderator`, `${message.author.tag} (${message.author.id}`)
+        .addField(`Moderator`, `${message.author.tag}` ("<@" + '${message.author.id}' + ">")
         .addField(`Banned Member`, `${Member.tag} (${Member.id})`)
         .addField(`Reason`, `${Reason || "No Reason Provided!"}`)
         .setFooter(`Requested by ${message.author.username}`)
--- commands/owner/dm.js
+++ commands/owner/dm.js
@@ -6,18 +6,10 @@ module.exports = {
   async run(client, message, args) {
     if (message.author.id !== owner) return;
     const dmMsg = args.join(" ");
-    const dm = new Discord.MessageEmbed()
-      //   const dm = new
-      //    const per =
-      //    message.mentions.users.first() || client.users.cache.get(args[0]);
-      //  if (per.id == "756052319417925633") return;
-      .setColor("RANDOM")
-      .setDescription(dmMsg)
-      .setTimestamp();
     message.delete();
 
     try {
-      message.mentions.users.first().send(dm);
+      message.mentions.users.first().send(dmMsg);
     } catch {
       message.channel.send("Error Sending DM");
     }
