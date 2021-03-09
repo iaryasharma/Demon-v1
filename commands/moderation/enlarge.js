465bef36ecc823c44f356d840332112bf0240692
Glitch (frag-nite-demon) <none>
2021-03-09 08:07:50 +0000

    👓⛺️ Checkpoint ./commands/moderation/enlarge.js:26900934/4558 ./package.json:26900934/56 ./commands/moderation/removerole.js:26900934/5700

---
+++ commands/moderation/enlarge.js
@@ -0,0 +1,82 @@
+const Discord = require("discord.js");
+const { prefix } = require("../../config.json");
+const { parse } = require("twemoji-parser");
+
+//const { prefix } = require('../config.json');
+
+module.exports = {
+  name: "enlarge",
+
+  aliases: ["viewemoji"],
+
+  description: "Adds Given Emoji To Server",
+
+  usage: `\`${prefix}enlarge <Emoji>\``,
+
+  run: async (client, message, args) => {
+    //   if (!message.guild.me.permissions.has("MANAGE_EMOJIS"))
+
+    //    return message.channel.send(
+
+    //       `I Don't Have Permission To Use This Command! Manage Emojis`
+
+    //      );
+
+    //    if (!message.member.permissions.has(`MANAGE_EMOJIS`))
+
+    //     return message.channel.send(
+
+    //       `You Don't Have Permission To Use This Command! Manage Emojis`
+
+    //      );
+
+    const emoji = args[0];
+
+    if (!emoji) return message.channel.send(`Usage: ${this.usage}`);
+
+    let customemoji = Discord.Util.parseEmoji(emoji);
+
+    if (customemoji.id) {
+      const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${
+        customemoji.animated ? "gif" : "png"
+      }`;
+
+      //     const name = args.slice(1).join(" ");
+
+      //     message.guild.emojis.create(
+
+      //       `${Link}`,
+      //       `${name || `${customemoji.name}`}`
+
+      //     );
+
+      const embed = new Discord.MessageEmbed()
+
+        .setTitle(`Emoji`)
+
+        .setColor(`GREEN`)
+
+        .setDescription(`Name : ${customemoji.name}`)
+
+        .setImage(Link)
+        .setFooter(
+          "Requested By : " + message.author.tag,
+
+          message.author.displayAvatarURL()
+        )
+
+        .setTimestamp((message.timestamp = Date.now()));
+
+      return message.channel.send(embed);
+    } else {
+      let CheckEmoji = parse(emoji, { assetType: "png" });
+
+      if (!CheckEmoji[0])
+        return message.channel.send(`Please Give Me A Valid Emoji!`);
+
+      message.channel.send(
+        `You Okay Using Default Emoji`
+      );
+    }
+  }
+};
--- commands/moderation/removerole.js
+++ commands/moderation/removerole.js
@@ -1,34 +1,152 @@
-const { MessageEmbed } = require('discord.js')
+//const { defprefix } = require('../botconfig.json');
+
+const Discord = require("discord.js");
 
 module.exports = {
-  name: "removerole",
-  aliases: ["rmrole", "-role"],
-  category: "moderation",
-  description: "Remove role from any user",
-  run: async (client, message, args) => {
-    
-    let target = message.mentions.members.first();
-    
-    if(!target) return message.reply(`I am unable to find the user`)
-    
-    let rrole = message.mentions.roles.first();
-    
-    if(!rrole) return message.reply(`I am unable to find the role`)
-    
-    let ticon = target.user.avatarURL({ dynamic: true, size: 2048 });
-    let aicon = message.author.avatarURL({ dynamic: true, size: 2048 });
-    
-      const embed = new MessageEmbed()
-      .setAuthor(target.user.username, ticon)
-      .setThumbnail(target.user.displayAvatarURL({ dynamic: true }))
-      .setColor("RANDOM")
-      .setDescription(`${rrole} role removed from ${target}`)
-      .setFooter(`Role added by ${message.author.username}`, aicon)
-      .setTimestamp()
-      
-      await message.channel.send(embed)
-      
-      target.roles.remove(rrole)
-    
+  name: "role-",
+  aliases: ["removerole", "rrole"],
+
+  description: "Removes Mentioned Role From The Mentioned User(s)",
+
+  async run(client, message, args) {
+    if (!message.guild.me.permissions.has("MANAGE_ROLES"))
+      return message.reply(
+        `I Don't Have Permission To Use This Command! Manage Roles`
+      );
+
+    if (!message.member.permissions.has("MANAGE_ROLES"))
+      return message.channel.send(
+        `You Don't Have Permission To Use This Command! Manage Roles`
+      );
+
+    if (!args[0])
+      return message.channel.send(
+        new Discord.MessageEmbed()
+
+          .setColor("RANDOM")
+
+          .addField("Role-", `\`role- <Role> <User(s)>\``, true)
+
+          .addField("Role- all", `\`role- all <Role>\``, true)
+
+          .addField("Role- bots", `\`role- bots <Role>\``, true)
+
+          .addField("Role- humans", `\`role- humans <Role>\``, true)
+
+          .setTimestamp()
+      );
+
+    switch (args[0]) {
+      case `bots`:
+        if (!args[1])
+          return message.channel.send(`\`Usage: role- bots <Role>\``);
+
+        const role1 =
+          message.mentions.roles.first() ||
+          message.guild.roles.cache.get(args[1]);
+
+        if (message.guild.me.roles.highest.position <= role1.position)
+          return message.reply(
+            `My Role isn't High Enough to Remove The Role! ${role1.name}`
+          );
+
+        if (message.member.roles.highest.position <= role1.position)
+          return message.reply(
+            `Your Role isn't High Enough to Remove The Role! ${role1.name}`
+          );
+
+        const botuser = message.guild.members.cache
+          .filter(member => member.user.bot)
+          .forEach(member => {
+            member.roles.remove(role1);
+          });
+
+        await message.channel.send(`Removed \`${role1.name}\` from All Bots.`);
+
+        break;
+
+      case `humans`:
+        if (!args[1])
+          return message.channel.send(`\`Usage: role- humans <Role>\``);
+
+        const role2 =
+          message.mentions.roles.first() ||
+          message.guild.roles.cache.get(args[1]);
+
+        if (message.guild.me.roles.highest.position <= role2.position)
+          return message.reply(
+            `My Role isn't High Enough to Remove The Role! ${role2.name}`
+          );
+
+        if (message.member.roles.highest.position <= role2.position)
+          return message.reply(
+            `Your Role isn't High Enough to Remove The Role! ${role2.name}`
+          );
+
+        const humanuser = message.guild.members.cache
+          .filter(member => !member.user.bot)
+          .forEach(member => {
+            member.roles.remove(role2);
+          });
+
+        await message.channel.send(
+          `Removed \`${role2.name}\` from All Humans.`
+        );
+
+        break;
+
+      case `all`:
+        if (!args[1])
+          return message.channel.send(`\`Usage: role+ all <Role>\``);
+
+        const role3 =
+          message.mentions.roles.first() ||
+          message.guild.roles.cache.get(args[1]);
+
+        if (message.guild.me.roles.highest.position <= role3.position)
+          return message.reply(
+            `My Role isn't High Enough to Remove The Role! ${role3.name}`
+          );
+
+        if (message.member.roles.highest.position <= role3.position)
+          return message.reply(
+            `Your Role isn't High Enough to Remove The Role! ${role3.name}`
+          );
+
+        const alluser = message.guild.members.cache.forEach(member => {
+          member.roles.remove(role3);
+        });
+
+        await message.channel.send(`Remove \`${role3.name}\` from Everyone.`);
+
+        break;
+
+      default:
+        let roleassign =
+          message.mentions.roles.first() ||
+          message.guild.roles.cache.get(args[0]);
+
+        if (roleassign.position > message.guild.me.roles.highest.position)
+          return message.reply(
+            `My Role isn't High Enough to Remove The Role! ${roleassign.name}`
+          );
+
+        if (message.member.roles.highest.position <= roleassign.position)
+          return message.reply(
+            `Your Role isn't High Enough to Remove The Role! ${roleassign.name}`
+          );
+
+        if (!roleassign) return message.reply(`No Such Role Exists .`);
+
+        const user = message.mentions.members.forEach(member => {
+          member.roles.remove(roleassign);
+        });
+
+        await message.channel.send(
+          `Remove ${roleassign.name} from ${message.mentions.members
+            .map(member => member.user.tag)
+            .join(", ")}.`
+        );
+    }
   }
-}
+};
