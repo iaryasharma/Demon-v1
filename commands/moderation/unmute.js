b851e59f3d5ae99770284a03513c255c131fe36a
Glitch (frag-nite-demon) <none>
2021-03-09 15:44:18 +0000

    🏙🐇 Checkpoint ./commands/moderation/mute.js:26900934/3309 ./commands/moderation/unmute.js:26900934/2555 ./commands/moderation/clear.js:26900934/6636

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -1,37 +1,52 @@
-const db = require("quick.db");
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
 
 module.exports = {
   name: "unmute",
-  category: "moderation",
+  aliases: [],
+  description: "Unmute A User!",
+  usage: "Unmute <Mention User>",
   run: async (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_ROLES")) {
+    //Start
+    message.delete();
+    if (!message.member.hasPermission("BAN_MEMBERS"))
       return message.channel.send(
-        "Sorry but you do not have permission to unmute anyone"
+        `You Don't Have Permission To Use This Command!`
       );
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]);
+
+    if (!Member) return message.channel.send(`Please Mention A User!`);
+
+    let Role = message.guild.roles.cache.find(role => role.name === "Muted").id;
+
+    if (!Role)
+      return message.channel.send(
+        `There Is No Mute Role, So Member Is Not Muted Anymore!`
+      );
+
+    if (!Member.roles.cache.has(Role)) {
+      return message.channel.send(`Member Is Already Unmuted!`);
     }
 
-    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
-      return message.channel.send("I do not have permission to manage roles.");
+    let Embed = new MessageEmbed()
+      .setColor("#ff0000")
+      .setTitle(`Member Unmuted!`)
+      .addField(`Moderator`, `<@${message.author.id}>`)
+      .addField(`Unmuted Member`, `<@${Member.user.id}>`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    if (Role && Member.roles.cache.has(Role)) {
+      Member.roles.remove([Role]);
+      return message.channel.send(Embed);
+    } else {
+      return message.channel.send(`Something Went Wrong, Try Again Later!`);
     }
 
-    const user = message.mentions.members.first();
-
-    if (!user) {
-      return message.channel.send("Please mention the member to who you want to unmute");
-    }
-
-    let muterole = message.guild.roles.cache.find(x => x.name === "Muted");
-
-    if (user.roles.cache.has(muterole)) {
-      return message.channel.send("Given User do not have mute role so what i am suppose to take");
-    }
-
-    user.roles.remove(muterole)
-
-    await message.channel.send(`**${message.mentions.users.first().username}** is now unmuted`);
-
-    user.send(`You are now unmuted from **${message.guild.name}**`);
-    
-    message.delete()
+    //End
   }
 };
