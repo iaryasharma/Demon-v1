d87898b07330d18932c0b55cd8d38362990044dc
Glitch (frag-nite-demon) <none>
2021-03-09 14:46:40 +0000

    👽🎌 Checkpoint ./commands/moderation/voicekick.js:26900934/4928

--- commands/moderation/voicekick.js
+++ commands/moderation/voicekick.js
@@ -4,8 +4,9 @@ module.exports = {
   name: "voicekick",
   aliases: ["vkick", "vckick", "voicekick"],
   description: "Kicks Mentioned User From Voice Channel",
+  category: "moderation",
   usage: `\`${prefix}voicekick <User>\``,
-  execute(bot, message, args) {
+  run: async (client, message, args) => {
     if (!message.guild.me.permissions.has("MOVE_MEMBERS"))
       return message.channel.send(
         "I Don't Have Permission To Use This Command! Move Members"
@@ -19,9 +20,9 @@ module.exports = {
       message.guild.members.cache.get(args[0]);
     if (!target) return message.channel.send(`Usage: ${this.usage}`);
 
-    let { channel } = message.mentions.members.first().voice.kick();
+    let { channel } = message.mentions.members.first().voice;
     if (!channel)
-      return message.channel.send(`User Is Not In Any Voice Channel!`);
+      return message.channel.send(`User Has Been Kicked From Voice Channel!`);
     if (target.id === `${owner}`)
       return message.channel.send(`I Cannot Voicekick My Owner!`);
     target.voice.kick();
@@ -29,4 +30,3 @@ module.exports = {
     message.channel.send(`User Has Been Kicked From Voice Channel!`);
   }
 };
-
