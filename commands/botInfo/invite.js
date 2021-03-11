dc17b85fbd8129a3a4c8473e34472afaf40862bb
Glitch (frag-nite-demon) <none>
2021-03-11 09:47:09 +0000

    🍵🎽 Checkpoint ./commands/botInfo/invite.js:26900934/196

--- commands/botInfo/invite.js
+++ commands/botInfo/invite.js
@@ -7,7 +7,7 @@ module.exports = {
     let embed = new discord.MessageEmbed()
       .setTitle(`HERE INVITE LINK OF BOT `)
       .setDescription(
-        `[INVITE BOT](https://discord.com/oauth2/authorize?client_id=795487199197528095&scope=bot&permissions=1933044991)`
+        `[INVITE BOT](https://discord.com/oauth2/authorize?client_id=795487199197528095&scope=bot&permissions=1933049087)`
       )
       .setColor("#00ff00")
       .setFooter(`Arya ØP`)
