e2d39904a9fab440fc826ac46db5bdf6a43384e6
Glitch (frag-nite-demon) <none>
2021-03-20 12:26:16 +0000

    🖇🚵 Checkpoint ./commands/fun/dicksize.js:26900934/1255 ./config.json:26900934/236

---
+++ commands/fun/dicksize.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.json");
+
+module.exports = {
+  name: "dicksize",
+  aliases: ["dick", "pp", "ppsize"],
+  description: "Show Member PP Size!",
+  usage: "Dicksize <Mention Member>",
+  run: async (client, message, args) => {
+    //Start
+    message.delete();
+    let sizes = [
+      "8D",
+      "8=D",
+      "8==D",
+      "8===D",
+      "8====D",
+      "8=====D",
+      "8======D",
+      "8=======D",
+      "8========D",
+      "8=========D",
+      "8==========D",
+      "8===========D",
+      "8============D",
+      "8=============D",
+      "8==============D",
+      "8===============D",
+      "8================D",
+      "8==================D"
+    ];
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+
+    let Result = sizes[Math.floor(Math.random() * sizes.length)];
+
+    let embed = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(`Pp v2 Machine`)
+      .setDescription(`${Member.user.username} pp Size Is\n${Result}`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+
+    //End
+  }
+};
