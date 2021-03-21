ae3487fd8f9deb6adb141fa30984df88cd101443
Glitch (frag-nite-demon) <none>
2021-03-21 04:17:12 +0000

    👨‍👨‍👦🐴 Checkpoint ./CODE_OF_CONDUCT.md:26900934/6735 ./LICENSE.md:26900934/1069

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -52,8 +52,8 @@ module.exports = {
       let embed = new discord.MessageEmbed()
         .setColor("#ff0000")
         .setTitle(`Member Banned!`)
-        .addField(`Moderator`, `${message.author.tag} (${message.author.id}`)
-        .addField(`Banned Member`, `${Member.tag} (${Member.id})`)
+        .addField(`Moderator`, `${message.author.tag} <@${message.author.id}>`)
+        .addField(`Banned Member`, `${Member.user.tag} <@${Member.user.id}>`)
         .addField(`Reason`, `${Reason || "No Reason Provided!"}`)
         .setFooter(`Requested by ${message.author.username}`)
         .setTimestamp();
