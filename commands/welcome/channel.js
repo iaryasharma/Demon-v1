3c06df6091ea71927e2f3a8d2db80fc19c568ee0
Glitch (frag-nite-demon) <none>
2021-03-22 16:44:19 +0000

    🛰🚨 Checkpoint ./commands/welcome/channel.js:26915577/1097

--- commands/welcome/channel.js
+++ commands/welcome/channel.js
@@ -8,7 +8,14 @@ module.exports = {
   description: "Set the welcome channel",
   run: (client, message, args) => {
     let channel = message.mentions.channels.first();
-
+    if (!message.guild.me.permissions.has("MANAGE_CHANNELS"))
+      return message.channel.send(
+        `I Don't Have Permission To Use This Command! Manage Channels`
+      );
+    if (!message.member.permissions.has("MANAGE_CHANNELS"))
+      return message.channel.send(
+        `You Don't Have Permission To Use This Command! Manage Channels`
+      );
     if (!channel) {
       return message.channel.send("Please Mention the channel first");
     }
