613e6d0843a0f6b82858ba384ad769b46593fb45
Glitch (frag-nite-demon) <none>
2021-04-05 15:10:42 +0000

    📰🌘 Checkpoint ./commands/botInfo/help.js:26915577/1042

--- commands/botInfo/help.js
+++ commands/botInfo/help.js
@@ -10,22 +10,24 @@ module.exports = {
       .setTitle(`<:Demon_2:822458701037174834> BOT  HELP MENU <:Demon_2:822458701037174834>`)
       .setDescription( `
 <:Demon_Moderation:828621978558529586> __**MODERATION COMMANDS**__ 
-\`addrole(give role to member),removerole(remove role from member),createrole(crole),createchannel(cchannel),addemoji,enlarge,whois,say,announce,clear[purge],ban,kick,voicekick,mute,unmute,slowmode,warn,warnings\`
+\`addrole(give role to member), removerole(remove role from member), createrole(crole), createchannel(cchannel), addemoji, enlarge, whois, say, announce, clear(purge), ban, kick, voicekick, mute, unmute, slowmode, warn, warnings
+\`
 
 <:Demon_Fun:828621911701323816> __**FUN COMMANDS**__ 
-\`advice,anime,ascii,cat,cry,dog,fact,hug,joke,kiss,dicksize(pp),howgay,meme,pat,punch,slap,say\`
+\`advice, anime, ascii, cat, cry, dog, fact, hug, joke, kiss, dicksize(pp), howgay, meme, pat, punch, slap\`
 
 <:Demon_Welcome:828622867796328470> __**WELCOME COMMANDS**__  
-\`setwelcome <#channel>,setwmessage,rsetwmessage(resets welcome message),wexample(welcome example)\`
+\`setwelcome <#channel>, setwmessage, rsetwmessage(resets welcome message), wexample(welcome example)\`
 
 <:Demon_Image:828622138260979802> __**UTILITY COMMANDS**__ 
-\`giveaway,math,weather,amongus,idp,avatar,serverinfo,hastebin,imdb,membercount,weather,pokemon,servericon\`
+\`giveaway, math, weather, amongus, idp, avatar, serverinfo, imdb, membercount, pokemon, servericon
+\`
 
 <:Demon_Others:828622145483178034> __**CORONA COMMANDS**__ 
 \`corona all, corona<country>\`
  
 <:Demon_Music1:828621262627799060> __**MUSIC COMMANDS**__
-\`music(for all music commands),play,leave,loop,lyrics,pause,resume,queue,skip,skipto <number>,volume <amount>\`
+\`music(for all music commands), play, leave, loop, lyrics, pause, resume, queue, skip, skipto <number>, volume <amount>\`
 
 <:Demon_Discord:828622083675914261> __**BOT INFO**__
 \`botinfo,support,invite,author,ping\`
