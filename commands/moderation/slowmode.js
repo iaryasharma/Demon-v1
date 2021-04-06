03ee394cfc65761f9848590f416cf60bd4e1fd48
Glitch (frag-nite-demon) <none>
2021-04-06 05:24:34 +0000

    🌞👽 Checkpoint ./commands/moderation/slowmode.js:26915577/3064

--- commands/moderation/slowmode.js
+++ commands/moderation/slowmode.js
@@ -1,27 +1,44 @@
+const { prefix } = require("../../config.json");
+const Discord = require("discord.js");
+
 module.exports = {
   name: "slowmode",
+  aliases: ["smode"],
   description: "Lets you set slowmode on the channel.",
+
   args: true,
-  usage: "<time>",
-  run: (client, message, args) => {
+
+  usage: `\`$prefix}slowmode <time>s/min/h\``,
+
+  async run(client, message, args) {
     const amount = parseInt(args[0]);
+
     if (message.member.hasPermission("MANAGE_CHANNEL"))
       if (isNaN(amount))
-        return message.channel.send("It doesn't seem to be valid number");
+        return message.channel.send(
+          "**Usage:**` " + prefix + "slowmode <time>s/min/h`"
+        );
+
     if (args[0] === amount + "s") {
       message.channel.setRateLimitPerUser(amount);
+
       if (amount > 1) {
         message.channel.send("slowmode is now " + amount + " seconds");
+
         return;
       } else {
         message.channel.send("slowmode is now " + amount + " second");
+
         return;
       }
     }
+
     if (args[0] === amount + "min") {
       message.channel.setRateLimitPerUser(amount * 60);
+
       if (amount > 1) {
         message.channel.send("slowmode is now " + amount + " minutes");
+
         return;
       } else {
         message.channel.send("slowmode is now " + amount + " minute");
@@ -29,18 +46,22 @@ module.exports = {
         return;
       }
     }
+
     if (args[0] === amount + "h") {
       message.channel.setRateLimitPerUser(amount * 60 * 60);
+
       if (amount > 1) {
         message.channel.send("slowmode is now " + amount + " hours");
+
         return;
       } else {
         message.channel.send("slowmode is now " + amount + " hour");
+
         return;
       }
     } else {
       message.channel.send(
-        "You can only set seconds(s), minutes(min) and hours(h)"
+        "**Usage:**  `" + prefix + "slowmode <time>s/min/h`"
       );
     }
   }
