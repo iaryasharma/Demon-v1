1ee10984fadd48bf43cbdf24943400256285fea9
Glitch (frag-nite-demon) <none>
2021-04-10 09:06:49 +0000

    🎌🏗 Checkpoint ./commands/voicemod/muteall.js:30036132/49 ./commands/voicemod/unmuteall.js:30036132/44

--- commands/voicemod/unmuteall.js
+++ commands/voicemod/unmuteall.js
@@ -13,7 +13,7 @@ module.exports = {
   for (let memberi of channel.members) {
     await memberi[1].voice.setMute(false);
   }
-  message.channel.send("<:Demon_VcUnmute:829767930451329085> Unmuted! Hope you won!!").then((msg) => {
+  message.channel.send("<:Demon_VcUnmute:829767930451329085> Unmuted everyone in Voice Channel").then((msg) => {
     b = msg.id;
   });
   await message.channel.messages.fetch(a).then(msg => msg.delete({ timeout: 1000 }));
