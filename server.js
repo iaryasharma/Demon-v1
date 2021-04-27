e174c6ec7082421bbcb7ae43485ed391ecbc9146
Glitch (frag-nite-demon) <none>
2021-04-27 03:57:50 +0000

    👩‍👩‍👦🛩 Checkpoint ./server.js:30904685/1856

--- server.js
+++ server.js
@@ -303,4 +303,58 @@ client.giveawaysManager.on(
   }
 );
 
+client.on("message", async message => {
+  function Check(str) {
+    if (
+      client.emojis.cache.find(emoji => emoji.name === str) ||
+      message.guild.emojis.cache.find(emoji => emoji.name === str)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (message.content.startsWith(":") && message.content.endsWith(":")) {
+    let EmojiName = message.content.slice(1, -1);
+
+    if (Check(EmojiName) === true) {
+      const channel = client.channels.cache.get(message.channel.id);
+      try {
+        let webhooks = await channel.fetchWebhooks();
+        let webhook = webhooks.first();
+        if (webhook === undefined || null || !webhook) {
+          let Created = channel
+            .createWebhook("SLAYER NITRO", {
+              avatar:
+                "https://cdn.discordapp.com/avatars/820213678493663262/09ff4d519159ee3f90472ce0604d9774.webp?size=2048"
+            })
+            .then(async webhook => {
+              const emoji =
+                client.emojis.cache.find(e => e.name == EmojiName).id ||
+                message.guild.emojis.cache.find(e => e.name === EmojiName).id;
+
+              await webhook.send(`${client.emojis.cache.get(emoji)}`, {
+                username: message.author.username,
+                avatarURL: message.author.avatarURL({ dynamic: true })
+              });
+              message.delete();
+            });
+        }
+
+        const emoji =
+          client.emojis.cache.find(e => e.name == EmojiName).id ||
+          message.guild.emojis.cache.find(e => e.name === EmojiName).id;
+
+        await webhook.send(`${client.emojis.cache.get(emoji)}`, {
+          username: message.author.username,
+          avatarURL: message.author.avatarURL({ dynamic: true })
+        });
+        message.delete();
+      } catch (error) {
+        console.log(`Error :\n${error}`);
+      }
+    }
+  }
+});
+
 client.login(process.env.TOKEN);
