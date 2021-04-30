5517029430f5063fd7f32c101b2bbfd9445545f0
Glitch (frag-nite-demon) <none>
2021-04-30 13:09:10 +0000

    📼🐦 Checkpoint ./commands/owner/leave.js:30904685/1689

--- commands/owner/leave.js
+++ commands/owner/leave.js
@@ -1,13 +1,13 @@
+const { prefix, owner } = require("../../config.json");
 module.exports = {
-    name: "leave",
+    name: "guildleave",
     group: "utility",
     description: "Leaves the server",
-        async run(client, message, args) {
-        try{
-            message.channel.send("bye :(")
-            message.guild.leave();
-        }catch(e) {
-            throw e;
+    run: async (client, message, args) => {
+        if (message.author.id !== '730424922639302693') {
+            return message.channel.send(`You cannot use this command!`)
         }
+        await message.channel.send(`Rleaving guild...`)
+      message.guild.leave();
     }
-}
+          }
