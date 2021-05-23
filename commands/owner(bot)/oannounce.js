f406986522675f7580a25b782449ca9c71ec2015
Glitch (op-demon-bot) <none>
2021-05-23 08:39:00 +0000

    🐪🖐 Checkpoint ./commands/owner(bot)/oannounce:26900934/1598 ./commands/owner(bot)/oannounce.js:26900934/980

---
+++ commands/owner(bot)/oannounce.js
@@ -0,0 +1,23 @@
+const Discord = require ('discord.js')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+name: "oannounce",
+aliases: ["oannounce", "oa"],
+category: "Utility",
+usage: "embed <text to say>",
+description: "Returns provided text in Embed form.",
+run: async(client, message, args) => {
+  if (message.author.id !== '730424922639302693') return;
+ await message.delete()
+  let say = message.content.split(" ").slice(1).join(" ")
+  if(!say) return message.channel.send(`❌ | `+"I Cannot Repeat Blank Message")
+  let embed = new MessageEmbed()
+.setAuthor(message.author.username, message.author.avatarURL())
+  .setDescription(`${say}`)
+  .setColor("#51ffae")
+.setFooter(` ${message.guild}`)
+.setTimestamp()
+  message.channel.send(embed)
+}
+}  
