8dffdb9b1d5f396bd2bc733ecee6ebb5c489264d
Glitch (op-demon-bot) <none>
2021-07-05 18:31:58 +0000

    👮🍬 Checkpoint ./commands/help/embed.js:26900934/2056 ./.glitch-assets:26900934/417

--- commands/help/embed.js
+++ commands/help/embed.js
@@ -1,172 +1,66 @@
-const Discord = require("discord.js");
+const discord = require("discord.js");
+const { prefix } = require("../../config.json");
 
 module.exports = {
-  name: "emb",
+  name: "embed-help",
 
-  aliases: ["embed-setup"],
+  aliases: ["embed"],
 
-  desciption: "say command",
+  category: "help",
 
-  category: "embed",
-
-  usage: "emb <message>",
+  description: "KNOW ABOUT THE CREATOR OF BOT",
 
   run: async (client, message, args) => {
+    let embed = new discord.MessageEmbed()
 
-      message.delete()
-      message.channel.send(
-        new Discord.MessageEmbed()
-        .setTitle(`Embed Setup | 1/7`)
-        .setDescription('What channel do you want your embed to be in?\nYou can cancel the setup at any time by saying \`cancel\`.')
-                      .setColor("#ff0073")
-        );
-      await startMessageCollectors(client, message, args);
-      function startMessageCollectors(client, message, args) {
-        let channelFilter = m => m.author.id === message.author.id;
-        let channelCollector = new Discord.MessageCollector(message.channel, channelFilter, { max: 999 });
-    
-        channelCollector.on('collect', async msg => {
-          let channel = await msg.mentions.channels.first();
-          if (msg.content.toLowerCase() === 'cancel') {
-                msg.channel.send('The embed setup has been cancelled.')
-            channelCollector.stop();
-            return;
-          }
-          if (!channel) {
-            await msg.channel.send('That is not a valid channel! Cancelled.');
-            await channelCollector.stop();
-            return;
-          } else {
-            msg.channel.send(
-              new Discord.MessageEmbed()
-              .setTitle(`Embed Setup | 2/7`)
-              .setDescription(`The embed will be in ${channel.toString()}. What title do you want for the embed?`)
-                      .setColor("#ff0073")
-            )
-            channelCollector.stop();
-          }
-          let titleFilter = m => m.author.id === message.author.id;
-          let titleCollector = new Discord.MessageCollector(message.channel, titleFilter, { max: 999 });
-          titleCollector.on("collect", async msg => {
-              let title = msg.content;
-            if (msg.content.toLowerCase() === 'cancel') {
-                msg.channel.send('The embed setup has been cancelled.')
-                channelCollector.stop();
-                return;
-              }
-              if(!title) {
-                  await msg.channel.send(`You didn't specify a title! Cancelled.`)
-                  await titleCollector.stop()
-              } else {
-                  msg.channel.send(
-                      new Discord.MessageEmbed()
-                      .setTitle(`Embed Setup | 3/7`)
-                      .setColor("#ff0073")
-                      .setDescription(`Thats's a nice title! now what color do you want?`)
-                  )
-                  titleCollector.stop()
-              }
-          let durationFilter = m => m.author.id === message.author.id;
-          let durationCollector = new Discord.MessageCollector(message.channel, durationFilter, { max: 999 });
-        durationCollector.on('collect', async msg => {
-            let duration = msg.content;
-            if (msg.content.toLowerCase() === 'cancel') {
-              msg.channel.send('The embed setup has been cancelled.')
-              durationCollector.stop();
-              return;
-            } else {
-              msg.channel.send(
-                new Discord.MessageEmbed()
-                      .setColor("#ff0073")
-                .setTitle(`Embed Setup | 4/7`)
-                .setDescription(`The color will be ${duration}, now what do you want the description to be?`)
-                );
-              durationCollector.stop();
-            }
-            let winnersFilter = m => m.author.id === message.author.id;
-            let winnersCollector = new Discord.MessageCollector(message.channel, winnersFilter, { max: 999 });
-        winnersCollector.on('collect', async msg => {
-            let trueWinners = msg.content;
+      .setTitle(
+        `<:Demon_2:822458701037174834> Embed HELP <:Demon_2:822458701037174834> `
+      )
+      
+      .addField(
+        "<:Demon_Arrow:828621113025363988> Embed Setup",
+        "`" +
+          prefix +
+          "embed-setup" `to start the setup process of Embed`
+      )
+      .addField(
+        "<:Demon_Arrow:828621113025363988>  Title of Embed",
+        `Bot will post an message asking to provide title of embed `
+      )
+      .addField(
+        "<:Demon_Arrow:828621113025363988>  Colour Of Embed",
+        `Bot will post an message asking to provide colour for the embed`
+      )
+      .addField(
+        "<:Demon_Arrow:828621113025363988>  Description Of Embed",
+        `Bot will post an message asking to provide Description for the embed (provide in approx 800 words)`
+      )
+      .addField(
+        "<:Demon_Arrow:828621113025363988>  Footer Of Embed",
+        `Bot will post an message asking to provide Footer for the embed`
+      )
+      .addField(
+        "<:Demon_Arrow:828621113025363988>  Timestamps For Embed",
+        `Bot will post an message asking if you want to add timestamp or not if Yes then type yes and if no then type No`
+      )
 
-            if (msg.content.toLowerCase() === 'cancel') {
-              msg.channel.send('The embed setup has been cancelled.')
-              winnersCollector.stop();
-              return;
-            } else {
-              msg.channel.send(
-                new Discord.MessageEmbed()
-                      .setColor("#ff0073")
-                .setTitle(`Embed Setup | 5/7`)
-                .setDescription(`OH what a nice description! Now, what do you want the footer to be?`)
-                )
-              winnersCollector.stop();
-            }
-            let prizeFilter = m => m.author.id === message.author.id;
-            let prizeCollector = new Discord.MessageCollector(message.channel, prizeFilter, { max: 999 });
-        prizeCollector.on('collect', async msg => {
-            let prize = msg.content;
-            if (msg.content.toLowerCase() === 'cancel') {
-              msg.channel.send('The embed setup has been cancelled.')
-              prizeCollector.stop();
-              return;
-            }
-            if (!prize) {
-              await msg.channel.send(`You didn't specify a footer! Cancelled!`)
-              prizeCollector.stop();
-              return;
-            } else {
-              msg.channel.send(
-                new Discord.MessageEmbed()
-                      .setColor("#ff0073")
-                .setTitle(`Embed Setup | 6/7`)
-                .setDescription(`What a cool footer! Do you want a timestamp? Type \`yes\` if you do, and type \`no\` if you don't.`)
-                );
-              prizeCollector.stop();
-            } 
-            let timeFilter = m => m.author.id === message.author.id;
-            let timeCollector = new Discord.MessageCollector(message.channel, timeFilter, { max: 999 });
-            timeCollector.on("collect", async msg => {
-                if (msg.content.toLowerCase() === 'cancel') {
-                    msg.channel.send('The embed setup has been cancelled.')
-                    prizeCollector.stop();
-                    return;
-                  }
-                  if (msg.content.toLowerCase() === "yes") {
-                    await msg.channel.send(
-                        new Discord.MessageEmbed()
-                      .setColor("#ff0073")
-                        .setTitle(`DONE!`)
-                        .setDescription(`There will be a timestamp. The embed has been sent in ${channel.toString()}.`)
-                    )
-                    timeCollector.stop()
-                    const embed2 = new Discord.MessageEmbed()
-                    .setTitle(title)
-                    .setColor(duration)
-                    .setDescription(trueWinners)
-                    .setFooter(prize)
-                    .setTimestamp()
-                    message.guild.channels.cache.get(channel.id).send(embed2)
-                  } else if(msg.content.toLowerCase() === "no"){
-                    msg.channel.send(
-                      new Discord.MessageEmbed()
-                      .setColor("#ff0073")
-                      .setTitle("DONE!")
-                      .setDescription(`There will be no timestamp. The embed has been sent in ${channel.toString()}.`)
-                      );
-                      const embed = new Discord.MessageEmbed()
-                      .setTitle(title)
-                      .setColor(duration)
-                      .setDescription(trueWinners)
-                      .setFooter(prize)
-                      message.guild.channels.cache.get(channel.id).send(embed)
-                    timeCollector.stop();
-                  }
-            })
-    })
-})
-    })
-})
-        })
-}
-}
+      .addField(
+        "<:Demon_Image:828622138260979802> Image",
+        "Below is an gif for better understanding"
+      )
+      .setImage(
+        "https://cdn.discordapp.com/attachments/811083347618037781/861674485752135680/ezgif-1-5d48382c70c6.gif"
+      )
+      .setColor("#ff0073")
+      .setThumbnail(client.user.avatarURL())
+      .setFooter(
+        "Requested By :-" + message.author.tag,
+
+        message.author.displayAvatarURL()
+      )
+
+      .setTimestamp((message.timestamp = Date.now()));
+
+    message.channel.send(embed);
+  }
 };
