df3da4e6394d4f8c4b7db81084212206d9c748c3
Glitch (op-demon-bot) <none>
2021-12-26 16:49:55 +0000

    🎉🎿 Checkpoint ./commands/owner(bot)/ocadd.js:26900934/1583

---
+++ commands/owner(bot)/ocadd.js
@@ -0,0 +1,38 @@
+const { prefix } = require("../../config.json");
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "create-chanel",
+  aliases: ["occhannel", "occh", "ocreatechannel", "ocadd"],
+  description: "Creates A Private Channel",
+  usage: `\`${prefix}cchannel <Name>\``,
+  run(client, message, args) {
+    if (!message.guild.me.permissions.has("MANAGE_CHANNELS"))
+      return message.channel.send(
+        `I Don't Have Permission To Use This Command! Manage Channels`
+      );
+    if (message.author.id !== '919296005378281472') return;
+    if (!args[0]) return message.channel.send(`Usage: ${this.usage}`);
+    const channelName = args[0];
+    message.guild.channels.create(channelName, {
+      type: "text",
+      position: 0,
+      permissionOverwrites: [
+        {
+          id: message.guild.id
+          //deny: ["SEND_MESSAGES", "VIEW_CHANNEL"]
+        }
+      ]
+    });
+    let embed = new Discord.MessageEmbed()
+      .setAuthor(
+        `${message.author.username} - (${message.author.id})`,
+        message.author.displayAvatarURL()
+      )
+      .setColor("RANDOM").setDescription(`
+Channel:  ${channelName.channel}
+By:  ${message.member}
+      `);
+    message.channel.send(embed);
+  }
+};
