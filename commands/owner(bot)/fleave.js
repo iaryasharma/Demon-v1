da01b515a28390ab1589fad45422cf26d1a661a4
Glitch (op-demon-bot) <none>
2022-01-02 05:41:01 +0000

    📒📖 Checkpoint ./commands/owner(bot)/fleave.js:26900934/7

--- commands/owner(bot)/fleave.js
+++ commands/owner(bot)/fleave.js
@@ -25,8 +25,7 @@ module.exports = {
 
     return guild.channels.cache.filter(c =>
       c instanceof TextChannel &&
-      c.permissionsFor(client.user.me)
-      .has([ 'VIEW_CHANNEL','SEND_MESSAGES' ]
+      c.permissionsFor(client.user.me).has([ 'VIEW_CHANNEL','SEND_MESSAGES' ]
     )).send(
       new MessageEmbed()
       .setColor('RED')
