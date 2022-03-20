adb21a8f55d232690f862e23b0cae08d902e67ce
Glitch (op-demon-bot) <none>
2022-03-20 03:33:53 +0000

    👢📇 Checkpoint ./json/badges.json:26900934/2 ./events/message.js:26900934/179332

--- events/message.js
+++ events/message.js
@@ -4,8 +4,7 @@ const discord = require("discord.js"),
   { bwebsite, binvite, bserver, pprefix, bowner } = require("../config.json"),
   apiPass = db.get("apipass"),
   apiKey = db.get("apikey"),
-  disbut = require("discord-buttons"),
-  Timeout = new discord.Collection();
+  disbut = require("discord-buttons");
 
 module.exports.run = async (client, message) => {
   if (message.author.bot || !message.guild) return;
@@ -18,9 +17,7 @@ module.exports.run = async (client, message) => {
     defprefix = nprefix;
   }
 
-  const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
-  if (message.content.match(prefixMention)) {
-    let btn1 = new disbut.MessageButton()
+  let btn1 = new disbut.MessageButton()
     .setStyle("url")
     .setLabel("|  SUPPORT")
     .setURL(bserver)
@@ -39,18 +36,30 @@ module.exports.run = async (client, message) => {
     .setURL(bwebsite)
     .setEmoji(client.emoji.dm_id)
     .setDisabled(false);
-  const btn4 = new disbut.MessageButton()
-    .setStyle("url")
-    .setLabel("|  VOTE")
-    .setURL(client.config.bvote)
-    .setEmoji(client.emoji.discord_id)
-    .setDisabled(false);
 
   const row = new disbut.MessageActionRow()
     .addComponent(btn1)
     .addComponent(btn2)
-    .addComponent(btn3)
-    .addComponent(btn4);
+    .addComponent(btn3);
+
+  let msg = `@here\nThank you for choosing **MARVEL**
+
+We hope marvel do help you with your server.
+You can join the support server by taping the support button below.
+
+You can also vote us ( https://top.gg/bot/748583869527097376/vote ) please do support our bot.
+
+**Note : ** 
+- Make sure not to delete this channel.
+- You can rename the channel.
+- you can categorize the channel.
+
+Add me to other servers by tapping the invite button.
+
+Thank you :heart:`
+
+  const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
+  if (message.content.match(prefixMention)) {
     let mention = new discord.MessageEmbed()
       .setAuthor(client.user.username, client.user.displayAvatarURL())
       .addField(
@@ -70,7 +79,7 @@ module.exports.run = async (client, message) => {
         "invite` - to invite the bot in youre server \n" +
         "`" +
         defprefix +
-        "vote` - to vote for the bot\n"
+        "author` to get the details about bot developer \n"
       )
       .addField(
         client.emoji.ar + "| " + "Prefix",
@@ -111,65 +120,383 @@ module.exports.run = async (client, message) => {
         cmd = args.shift().toLowerCase();
       if (cmd.length === 0) return;
 
+      if (cmd === "setup") {
+        if (
+          !message.guild.me
+            .permissionsIn(message.channel)
+            .has("MANAGE_CHANNELS") && !message.guild.me
+              .permissionsIn(message.channel)
+              .has("MANAGE_ROLES")
+        ) {
+          return message
+            .lineReply(
+              client.emoji.fail +
+              "| I NEED **`MANAGE_CHANNELS, MANAGE_ROLES`** PERMISSIONS FIRST TO EXECUTE THIS COMMAND!!"
+            )
+            .then((m) => m.delete({ timeout: 3000 }));
+        }
+        const setup = db.get("setup" + message.channel.guild.id),
+          setchan = message.guild.channels.cache.get(setup);
+
+        if (
+          setup === null ||
+          setup === undefined ||
+          !setup ||
+          !setchan ||
+          setchan === null ||
+          setchan === undefined
+        ) {
+          let c = await message.guild.channels
+            .create("marvel-private")
+            .then((channel) => {
+              channel.updateOverwrite(client.user, {
+                VIEW_CHANNEL: true,
+                SEND_MESSAGES: true,
+                EMBED_LINKS: true,
+              });
+              channel.updateOverwrite(message.guild.roles.everyone, {
+                VIEW_CHANNEL: false,
+              });
+              channel.send(msg, {
+                components: [row],
+              })
+              message.lineReply(
+                new discord.MessageEmbed({
+                  description:
+                    client.emoji.success +
+                    "| <#" +
+                    channel.id +
+                    "> Is created and set as update channel",
+                  color: client.embed.cr,
+                })
+              );
+              db.set("setup" + message.guild.id, channel.id);
+              return;
+            });
+        } else {
+          return message.lineReply(
+            new discord.MessageEmbed({
+              description:
+                client.emoji.fail +
+                "| <#" +
+                setchan.id +
+                "> Is already set as update channel in this guild make sure to give me permissions in that channel",
+              color: client.embed.cf,
+            })
+          );
+        }
+      }
+
       let command = client.commands.get(cmd);
       if (!command) command = client.commands.get(client.aliases.get(cmd));
       if (command) {
-          const heis = client.guilds.cache.get("739419850614767707");
-          let inside = heis.members.cache.get(message.author.id);
-          if (!inside) {
-            inside = await heis.fetchMember(message.author.id)
+        let r = false;
+        const modOnly = db.get("modOnly" + message.guild.id);
+        if (!bowner.includes(message.member.id)) {
+          if (modOnly === true) {
+            if (
+              !message.member.permissionsIn(message.channel).has("ADMINISTRATOR")
+            )
+              return message
+                .lineReply(
+                  client.emoji.fail + "| Bot is mod only in this guild"
+                )
+                .then((m) => m.delete({ timeout: 3000 }));
           }
-          if (!inside) {
-            let join = new discord.MessageEmbed({
-              description:
-                "You need to join my support server first.\n" +
-                "In order to use no prefix",
-              color: client.color.cf
-            })
-            const np = new disbut.MessageButton()
-              .setStyle("url")
-              .setLabel("|  SERVER")
-              .setURL(client.config.bserver)
-              .setEmoji(client.emoji.discord_id)
-              .setDisabled(false)
-            const row = new disbut.MessageActionRow()
-              .addComponent(np);
-            return message.channel
-              .send(join, {
-                components: [row],
-              });
+          command.userPermissions.forEach((permission) => {
+            if (r === true) return;
+            if (
+              !message.member.permissionsIn(message.channel).has(permission)
+            ) {
+              r = true;
+              return message.lineReply(
+                client.emoji.fail +
+                "| YOU NEED **`" +
+                permission +
+                "`** PERMISSION FIRST TO EXECUTE THIS COMMAND!!"
+              ).then((m) => m.delete({ timeout: 3000 }));
+            }
+          });
+        }
+        command.botPermissions.forEach((permission) => {
+          if (r === true) return;
+          if (
+            !message.guild.me.permissionsIn(message.channel).has(permission)
+          ) {
+            r = true;
+            return message.lineReply(
+              client.emoji.fail +
+              "| I NEED **`" +
+              permission +
+              "`** PERMISSION FIRST TO EXECUTE THIS COMMAND!!"
+            ).then((m) => m.delete({ timeout: 3000 }));
           }
-        if (command.vote === true) {
-          let vote = new discord.MessageEmbed({
-            description: "You need to vote first to use this command.",
-            color: client.color.cf
-          })
-          const vb = new disbut.MessageButton()
-            .setStyle("url")
-            .setLabel("|  VOTE")
-            .setURL(client.config.bvote)
-            .setEmoji(client.emoji.discord_id)
-            .setDisabled(false)
-          const row = new disbut.MessageActionRow()
-            .addComponent(vb);
-          let pre = db.get("voted" + message.author.id);
-          if (pre !== true) return message.channel
-            .send(vote, {
-              components: [row],
-            });
-          const trt = db.get("vote-time_" + message.author.id);
-          var milliseconds = trt;
-          var millisecondsInDay = 8.64e7;
-          var futureDate = new Date(milliseconds + 1 * millisecondsInDay);
-          var tit = Date.now();
-          if (futureDate - tit <= 0) {
-            return (
-              message.channel
-                .send(vote, {
-                  components: [row],
-                }) &&
-              db.delete("votes" + message.author.id) &&
-              db.delete("vote-time_" + message.author.id)
+        });
+        if (r === false) {
+          try {
+            command.run(client, message, args)
+          } catch (e) {
+            console.log(e)
+            client.web.send(
+              "```js\n" + e.message + "```\n```" + command.name + "```"
             );
           }
         }
+      }
+    } catch (e) {
+      console.log(e);
+      client.web.send("```js\n" + e.message + "```");
+    }
+  }
+
+  // For user with prefix  
+  // normal users
+
+  if (message.content.startsWith(defprefix)) {
+    try {
+      const modOnly = db.get("modOnly" + message.guild.id);
+      if (!message.member)
+        message.member = await message.guild.fetchMember(message);
+      const args = message.content.slice(defprefix.length).trim().split(/ +/g);
+      const cmd = args.shift().toLowerCase();
+      if (cmd.length === 0) return;
+      if (cmd === "setup") {
+        if (!message.member.permissionsIn(message.channel).has("ADMINISTRATOR")
+        ) {
+          return message
+            .lineReply(
+              client.emoji.fail +
+              "| YOU NEED **`ADMINISTRATOR`** PERMISSION FIRST TO EXECUTE THIS COMMAND!!"
+            )
+            .then((m) => m.delete({ timeout: 3000 }));
+        }
+        if (
+          !message.guild.me
+            .permissionsIn(message.channel)
+            .has("MANAGE_CHANNELS") && !message.guild.me
+              .permissionsIn(message.channel)
+              .has("MANAGE_ROLES")
+        ) {
+          return message
+            .lineReply(
+              client.emoji.fail +
+              "| I NEED **`MANAGE_CHANNELS`** PERMISSION FIRST TO EXECUTE THIS COMMAND!!"
+            )
+            .then((m) => m.delete({ timeout: 3000 }));
+        }
+        const setup = db.get("setup" + message.channel.guild.id),
+          setchan = message.guild.channels.cache.get(setup);
+
+        if (
+          setup === null ||
+          setup === undefined ||
+          !setup ||
+          !setchan ||
+          setchan === null ||
+          setchan === undefined
+        ) {
+          let c = await message.guild.channels
+            .create("marvel-private")
+            .then((channel) => {
+              channel.updateOverwrite(client.user, {
+                VIEW_CHANNEL: true,
+                SEND_MESSAGES: true,
+                EMBED_LINKS: true,
+              });
+              channel.updateOverwrite(message.guild.roles.everyone, {
+                VIEW_CHANNEL: false,
+              });
+              channel.send(msg, {
+                components: [row],
+              })
+              message.lineReply(
+                new discord.MessageEmbed({
+                  description:
+                    client.emoji.success +
+                    "| <#" +
+                    channel.id +
+                    "> Is created and set as update channel",
+                  color: client.embed.cr,
+                })
+              );
+              db.set("setup" + message.guild.id, channel.id);
+              return;
+            });
+        } else {
+          return message.lineReply(
+            new discord.MessageEmbed({
+              description:
+                client.emoji.fail + "| <#" + setchan.id +
+                "> Is already set as update channel in this" +
+                " guild make sure to give me permissions in that channel",
+              color: client.embed.cf,
+            })
+          );
+        }
+      }
+      let command = client.commands.get(cmd);
+      if (!command) command = client.commands.get(client.aliases.get(cmd));
+      if (command) {
+        const setup = db.get("setup" + message.channel.guild.id);
+
+        const setchan = message.guild.channels.cache.get(setup);
+        if (
+          setup === null ||
+          setup === undefined ||
+          !setup ||
+          !setchan ||
+          setchan === null ||
+          setchan === undefined
+        ) {
+          return message.lineReply(
+            new discord.MessageEmbed({
+              description:
+                client.emoji.fail + "| Please run `" +
+                defprefix + "setup` command first!\nI'll create a channel "
+                + "for updates make sure your security bot don't kick me!",
+              color: client.embed.cf,
+            })
+          );
+        }
+
+        if (dis === true) {
+          return message.react(client.emoji.fail)
+        }
+
+        const talkedRecently = db;
+        const wait = db.get(`cooldown_${message.guild.id + message.author.id}`);
+        let cooldown = 5000;
+
+        if (wait !== null && cooldown - (Date.now() - wait) > 0) {
+          let timeObj = ms(cooldown - (Date.now() - wait));
+          return message
+            .lineReply(
+              "**Wait __`" + timeObj + "`__ Before Using My Commands Again**"
+            )
+            .then((m) => m.delete({ timeout: 5000 }));
+        }
+        let r = false;
+        if (!bowner.includes(message.member.id)) {
+          if (modOnly === true) {
+            if (
+              !message.member.permissionsIn(message.channel).has("ADMINISTRATOR")
+            )
+              return message
+                .lineReply(
+                  client.emoji.fail + "| Bot is mod only in this guild"
+                )
+                .then((m) => m.delete({ timeout: 3000 }));
+          }
+
+          command.userPermissions.forEach((permission) => {
+            if (r === true) return;
+            if (
+              !message.member.permissionsIn(message.channel).has(permission)
+            ) {
+              r = true;
+              return message.lineReply(
+                client.emoji.fail +
+                "| YOU NEED **`" +
+                permission +
+                "`** PERMISSION FIRST TO EXECUTE THIS COMMAND!!"
+              ).then((m) => m.delete({ timeout: 3000 }));
+            }
+          });
+        }
+        command.botPermissions.forEach((permission) => {
+          if (r === true) return;
+          if (
+            !message.guild.me.permissionsIn(message.channel).has(permission)
+          ) {
+            r = true;
+            return message.lineReply(
+              client.emoji.fail +
+              "| I NEED **`" +
+              permission +
+              "`** PERMISSION FIRST TO EXECUTE THIS COMMAND!!"
+            ).then((m) => m.delete({ timeout: 3000 }));
+          }
+        });
+        if (r === false) {
+          try {
+            command.run(client, message, args) &&
+              talkedRecently.set(
+                `cooldown_${message.guild.id + message.author.id}`,
+                Date.now()
+              );
+            setTimeout(() => {
+              talkedRecently.delete(
+                `cooldown_${message.guild.id + message.author.id}`
+              );
+            }, cooldown);
+          } catch (e) {
+            console.log(e)
+            client.web.send(
+              "```js\n" + e.message + "```\n```" + command.name + "```"
+            );
+          }
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      client.web.send("```js\n" + e.message + "```");
+    }
+  }
+
+  // announcement system starts here
+  // for marvel private channels across the servers
+
+  if (message.channel.id === client.config.announce) {
+    if (!client.config.bowner.includes(message.author.id)) {
+      return;
+    }
+    const ok = new disbut.MessageButton()
+      .setStyle("green")
+      .setEmoji(client.emoji.tick_id)
+      .setID("okay")
+      .setDisabled(false);
+
+    const cancel = new disbut.MessageButton()
+      .setStyle("red")
+      .setEmoji(client.emoji.cross_id)
+      .setID("cancel")
+      .setDisabled(false);
+
+    const row = new disbut.MessageActionRow()
+      .addComponent(ok)
+      .addComponent(cancel);
+
+    message.channel
+      .send(message.content, {
+        components: [row],
+      })
+      .then((m) => {
+        client.on("clickButton", async (button) => {
+          try {
+            if (button.clicker.id !== message.author.id) return;
+            if (button.message.id !== m.id) return;
+            if (button.id == "cancel") {
+              return m.delete();
+            } else if (button.id === "okay") {
+              m.delete();
+              client.guilds.cache.forEach((guild) => {
+                const setup = db.get("setup" + guild.id)
+                if (!setup || setup === null) return
+                const setchan = guild.channels.cache.get(setup);
+                try {
+                  setchan.send(message.content)
+                  console.log(guild.name + ' Sent')
+                } catch (e) {
+                  return db.delete("setup" + guild.id)
+                }
+              });
+            } else {
+              return;
+            }
+          } catch (e) {
+            console.log(e);
+          }
+        });
+      });
+  }
+};
